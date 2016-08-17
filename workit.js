72b211c9c67d9f9108f8947391c45586c5320e3e
Christophe <christophe@ksdc.be>
2016-08-17 23:27:18 +0200

    Added a timer, cosmetic changes

--- workit.js
+++ workit.js
@@ -1,4 +1,7 @@
  var hls = null;
+ var timeoutId = null;
+ var countDownId = null;
+ var destructionText = document.getElementById("destructionText");
  
  // hls 304 workaround. See: https://github.com/dailymotion/hls.js/issues/615
  
@@ -16,10 +19,12 @@
   debug:false
 }
  
+ // This is the main play function
+
  function playSporzaVid(id){
 	var url = "http://ovpvrt1-live.hls.adaptive.level3.net/vrt/channel0" + id + "/1.m3u8"
 	// console.log(url);
-	
+	// TODO: yeah. Maybe refactor this one.
 	switch (id){
 		case 1:
 			url = "http://ovpvrt1-live.hls.adaptive.level3.net/vrt/channel01/1.m3u8";
@@ -46,11 +51,13 @@
 			console.log('destroying');
 			hls.destroy();
 		}
-		console.log("creating new hls");
+		// console.log("creating new hls");
+
+		var video = document.getElementById('video');
 		hls = new Hls(config);
 		
 		hls.attachMedia(video);
-		console.log("twice?");
+		// console.log("twice?");
 		hls.on(Hls.Events.MEDIA_ATTACHED,function(event, data){
 			hls.loadSource(url);
 			hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
@@ -59,40 +66,111 @@
 				video.play();
 			});
 		})
-		console.log("twice?");
 	}
  }
 
-if(Hls.isSupported()) {
-	console.log("Setting Hls event handlers");
-	var video = document.getElementById('video');
-}
+// Check if there is a video already set (usually means a refresh of the tab)
 
- if (window.location.hash){
+if (window.location.hash){
 	playSporzaVid(window.location.hash.substring(1,2));
- }else {
+} else {
 	playSporzaVid(1);
- }
+}
  
- function buttonHandler(event){
+//
+// Get all video links and add event handlers
+//
+
+var nodes = document.getElementsByClassName("streamLink");
+
+function buttonHandler(event){
 	//console.log(event)
 	if(event.target && event.target.attributes) {
 		playSporzaVid(parseInt(event.target.attributes["data-id"].value));
 	}
+	if(!timeoutId) {
+		warnForDestruction();
+	}
 }
- 
-var nodes = document.getElementsByClassName("streamLink");
- 
+
 for (var i = 0; i < nodes.length; i++) {
 	nodes[i].addEventListener("click", buttonHandler)
 }
 
-function stopHandler() {
-		if(hls){
-			hls.destroy();
-		}
-}
+//
+// this destroys the current hls instance
+//
 
 var stop = document.getElementById("stopLink");
 
+function stopHandler() {
+	if(hls){
+		hls.destroy();
+	}
+}
+
 stop.addEventListener("click", stopHandler);
+
+//
+// the next section defines the automatic stop
+//
+
+var stopButton = document.getElementById("destroyButton");
+
+function processDestruction(){
+	var setTime = parseInt(document.getElementById("setDestroyTime").value);
+	if(timeoutId) {
+		resetTimer(timeoutId);
+	}
+	if(setTime !== 0){
+		displayCountdown(setTime);
+		timeoutId = setTimeout(function(){
+			console.log("Destruction initiated!");
+			//stopHandler();
+			timeoutId = null;
+		}, setTime * 1000);
+		console.log("Destruction set", timeoutId, setTime);
+	}else {
+		if(countDownId){
+			resetCountDown(countDownId, true);
+		}
+	}
+}
+
+function displayCountdown(countDownTime){
+	if(countDownId){
+		resetCountDown(countDownId);
+	}
+	destructionText.innerHTML = "Video will stop in: " + countDownTime;
+	destructionText.classList.remove("showMeRed");
+	countDownId = setInterval(function(){
+		countDownTime--;
+		if(countDownTime == 0){
+			destructionText.innerHTML = "Video stopped. Bandwidth safe!";
+			resetCountDown(countDownId);
+		} else{
+			destructionText.innerHTML = "Video will stop in: " + countDownTime;
+		}
+	}, 1000);
+}
+
+function resetCountDown(countDownId, stopflag = false){
+	clearTimeout(countDownId);
+	countDownId = null;
+	if (stopflag){
+		warnForDestruction()	
+	}
+}
+
+function resetTimer(timeoutId){
+	clearTimeout(timeoutId);
+	timeoutId = null;
+	console.log("Destruction avoided, for now");
+}
+
+function warnForDestruction(){
+	destructionText.innerHTML = "Automatic stop is not set, mind your bandwidth!";
+	destructionText.classList.add("showMeRed");
+}
+
+stopButton.addEventListener("click", processDestruction);
